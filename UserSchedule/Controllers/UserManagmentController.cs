570a52e2d72f47cc2065355e50135dbbbeb9fc34
anetegithub <anete.anetes@gmail.com>
2015-11-30 22:43:01 +0500

    Front-end template

---
+++ UserSchedule/Controllers/UserManagmentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserSchedule.Controllers
+{
+    public class UserManagmentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Read()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ UserSchedule/Scripts/App.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="jquery-1.9.1.js" />
+/// <reference path="linq.js" />
+$(document).ready(function () {
+    var SelectedMenu = $('#selectedMenu').val();
+    var Element = Enumerable.From($('.navbar-nav > li > a').toArray())
+        .Where(function (x) {
+            return $(x).html() == SelectedMenu;
+        })
+        .ToArray()[0];
+    $(Element).parent().addClass('active');
+})
